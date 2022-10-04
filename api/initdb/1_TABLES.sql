1dc0cee2e863807d776739b7b603e204ef60fee1
Marion MONANGE <marion.monange@epfedu.fr>
2022-10-04 10:25:40 +0200

    Modify 1_TABLES.sql to get right variables types

--- api/initdb/1_TABLES.sql
+++ api/initdb/1_TABLES.sql
@@ -1,34 +1,37 @@
+drop table users;
 create table users
 (
     id bigint auto_increment,
     constraint users_pk
         primary key (id),
-    first_name TEXT not null,
-    last_name TEXT not null,
+    first_name VARCHAR(255) not null,
+    last_name VARCHAR(255) not null,
     age int null
 );
 
+drop table kittens;
 create table kittens
 (
     id bigint auto_increment,
     constraint kittens_pk
         primary key (id),
-    first_name TEXT not null,
-    fur TEXT not null,
-    race TEXT not null,
+    first_name VARCHAR(255) not null,
+    fur VARCHAR(255) not null,
+    race VARCHAR(255) not null,
     age int null,
-    imagepath TEXT not null,
-    sex TEXT not null
+    imagepath VARCHAR(255) not null,
+    sex VARCHAR(255) not null
 );
 
+drop table reviews;
 create table reviews
 (
     id bigint auto_increment,
     constraint reviews_pk
         primary key (id),
-    date TEXT not null,
+    date VARCHAR(255) not null,
     rating int null,
-    text TEXT not null,
-    userid LONG null,
-    kittenid LONG null
+    text VARCHAR(255) not null,
+    userid bigint null,
+    kittenid bigint null
 );
