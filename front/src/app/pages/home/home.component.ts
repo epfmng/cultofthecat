40b3caa5b46c5d0932c99834d738a18f8c784c74
CorentinDau <cocodaubert@gmail.com>
2022-10-10 14:32:17 +0200

    [front] Page Home marche mais pas bo et lien vers jugé à changé

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,57 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Kitten} from '../../models/kitten.model'
 import {KittenService} from '../../services/kitten.service';
-
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  kittens : Kitten[];
-  kittenPrint : Kitten;
-  constructor(private kittenService : KittenService) { }
+    kittens: Kitten[];
+    kittenPrint: Kitten;
 
-  ngOnInit(): void {
-    this.kittenService.getKittens().subscribe(kittens => {
-      this.kittens = kittens;
-      console.log(this.kittens)
-        this.loadKitten();
-    });
-  }
-
-
-   loadKitten(): void{
-    if (this.kittenPrint!= null){
-    do {
-      var newId = this.kittens[this.randNum()].id;
-    }while (newId == this.kittenPrint.id);
+    constructor(private kittenService: KittenService, private router : Router) {
     }
 
-    var idKitten=this.kittens[this.randNum()].id;
-    this.kittenService.getKitten(idKitten)
-        .subscribe(kitten => {
-            this.kittenPrint = kitten
-            console.log(this.kittenPrint);
+    ngOnInit(): void {
+        this.kittenService.getKittens().subscribe(kittens => {
+            this.kittens = kittens;
+            console.log(this.kittens)
+            this.loadKitten();
         });
-  }
+    }
 
-  randNum(): number{
-    var min = 1;
-    var max = this.kittens.length;
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
 
-  nextKitten(): void{
-    this.loadKitten();
-  }
+    loadKitten(): void {
+        if (this.kittenPrint != null) {
+            do {
+                var idKitten = this.kittens[this.randNum()].id;
+            } while (idKitten == this.kittenPrint.id);
+        } else {
+            var idKitten = this.kittens[this.randNum()].id;
+        }
 
-  judgeKitten(): Kitten{
-    return this.kittenPrint;
-  }
+        this.kittenService.getKitten(idKitten)
+            .subscribe(kitten => {
+                this.kittenPrint = kitten
+                console.log(this.kittenPrint);
+            });
+    }
+
+    randNum(): number {
+        var min = 1;
+        var max = this.kittens.length;
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    nextKitten(): void {
+        this.loadKitten();
+    }
+
+    judgeKitten(): void{
+        this.router.navigateByUrl('kittens')
+    }
 
 }
