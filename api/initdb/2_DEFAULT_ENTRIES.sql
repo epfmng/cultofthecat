9399cecae7b0c9c7215c70e031b6f1af395701c7
Marion MONANGE <marion.monange@epfedu.fr>
2022-10-26 20:43:48 +0200

    [back] Update the kittens table and the kittens pictures

--- api/initdb/2_DEFAULT_ENTRIES.sql
+++ api/initdb/2_DEFAULT_ENTRIES.sql
@@ -9,16 +9,16 @@ INSERT INTO defaultdb.users (id, first_name, last_name, age, email) VALUES (8, '
 INSERT INTO defaultdb.users (id, first_name, last_name, age, email) VALUES (9, 'Thomas',  'Ouaque',      null, 'touaque@epf.fr');
 INSERT INTO defaultdb.users (id, first_name, last_name, age, email) VALUES (10, 'Sacha',  'Telfrize',    23,   'stelfrize@epf.fr');
 
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (1, 'Paul',    'white',            'Unknown', 4,  'Kitten_1.jpg', 'female');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (2, 'Harry',   'white',            'Unknown', 4,  'Kitten_2.jpg', 'male');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (3, 'Alain',   'black',            'Unknown', 4,  'Kitten_3.jpg', 'female');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (4, 'Elvire',  'brown',            'Unknown', 4,  'Kitten_4.jpg', 'male');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (5, 'Laurent', 'yellow',           'Unknown', 6,  'Kitten_.jpg', 'female');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (6, 'Homer',   'grey',             'Unknown', 6,  'Kitten_6.jpg', 'male');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (7, 'Gaston',  'ginger',           'Unknown', 6,  'Kitten_7.jpg', 'female');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (8, 'Gisèle',  'black',            'Unknown', 6,  'Kitten_8.jpg', 'male');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (9, 'Thomas',  'black and white',  'Unknown', 2,  'Kitten_9.jpg', 'female');
-INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (10, 'Sacha',  'brown and black',  'Unknown', 2,  'Kitten_10.jpg', 'male');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (1, 'Saumon',    'marmelade',            'Unknown', 4,  'Kitten_1.jpg', 'female');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (2, 'Biscotte',   'brown',            'Unknown', 4,  'Kitten_2.jpg', 'male');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (3, 'Chamallow',   'white',            'Unknown', 4,  'Kitten_3.jpg', 'female');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (4, 'Raclette',  'brown',            'Unknown', 4,  'Kitten_4.jpg', 'male');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (5, 'Champignon', 'black',           'Unknown', 6,  'Kitten_5.jpg', 'female');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (6, 'Tartine',   'grey',             'Unknown', 6,  'Kitten_6.jpg', 'male');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (7, 'Ramen',  'white',           'Unknown', 6,  'Kitten_7.jpg', 'female');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (8, 'Carotte',  'black',            'Unknown', 6,  'Kitten_8.jpg', 'male');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (9, 'Framboise',  'black and white',  'Unknown', 2,  'Kitten_9.jpg', 'female');
+INSERT INTO defaultdb.kittens (id, first_name, fur, race, age, imagepath, sex) VALUES (10, 'Bonbon',  'tortoiseshell',  'Unknown', 2,  'Kitten_10.jpg', 'male');
 
 INSERT INTO defaultdb.reviews (id, date, rating, text, userid, kittenid) VALUES (1,  '01/09/2003',  3, 'white',            1,  1);
 INSERT INTO defaultdb.reviews (id, date, rating, text, userid, kittenid) VALUES (2,  '01/09/2003',  3, 'white',            6,  6);
