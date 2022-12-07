fa238fde2968d3a541a793b4cbca43c1d81145d1
Marion MONANGE <marion.monange@epfedu.fr>
2022-12-07 12:31:19 +0100

    Temp

--- test1.sh
+++ test1.sh
@@ -21,7 +21,7 @@ sudo mkdir -p /etc/apt/keyrings
 #  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 #  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 #sudo docker run hello-world
 
 #sudo apt-get install nodejs
